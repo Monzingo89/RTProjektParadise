0e0b3fe1ee6aefe56bc44a85382fe0f0bb43d15a
Robert Justin Monzingo <robertjmonzingo@gmail.com>
2022-04-21 13:31:16 -0500

    adding changes to repo

---
+++ projekt-paradise/src/components/Counter/index.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import { useValues, useActions } from 'kea'
+import { counterLogic } from './counterLogic'
+import './style.css'
+
+export const Counter = () => {
+    return (
+        <div>
+            <div>
+               
+            </div>
+            <br />
+            <div>
+
+            </div>
+        </div>
+    )
+}
