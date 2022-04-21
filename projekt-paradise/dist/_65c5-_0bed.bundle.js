817a734d1d31bd0534cb06bf7706730173a0e7f8
Robert Justin Monzingo <robertjmonzingo@gmail.com>
2022-04-21 13:46:15 -0500

    dont ignore dist and add index

---
+++ projekt-paradise/dist/_65c5-_0bed.bundle.js
@@ -0,0 +1,31 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkreact_scratch"] = self["webpackChunkreact_scratch"] || []).push([["_65c5-_0bed"],{
+
+/***/ "?65c5":
+/*!********************!*\
+  !*** fs (ignored) ***!
+  \********************/
+/***/ (() => {
+
+/* (ignored) */
+
+/***/ }),
+
+/***/ "?0bed":
+/*!**********************!*\
+  !*** util (ignored) ***!
+  \**********************/
+/***/ (() => {
+
+/* (ignored) */
+
+/***/ })
+
+}]);
